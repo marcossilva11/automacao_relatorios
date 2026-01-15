152d9e73427ce8c34ee3f334f11a10433925ba71
marcossilva11 <marcosant2170@gmail.com>
2026-01-15 15:00:07 -0300

    feat: adicionar camada de configuracao do projeto

---
+++ config/settings.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings:
+  DB_DRIVER = os.getenv("DB_DRIVER")
+  DB_SERVER = os.getenv("DB_SERVER")
+  DB_DATABASE = os.getenv("DB_DATABASE")
+  DB_USERNAME = os.getenv("DB_USERNAME")
+  DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+settings = Settings()
